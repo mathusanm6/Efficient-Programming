b23fb735868d5b49cf6f1f279d8e6dedd5da212a
selvakum <mathusan.selvakumar@etu.u-paris.fr>
2024-04-16 20:30:20 +0200

    fix(judge): Stress test error

--- judge.sh
+++ judge.sh
@@ -32,7 +32,9 @@ blue() {
 judge_one() {
     cd "$1" || exit
     blue "Judging $1"
-    judge "$PWD"
+    if ! judge "$PWD"; then
+        return 1
+    fi
     cd - || exit
 }
 
@@ -81,22 +83,36 @@ judge() {
 }
 
 judge_all() {
+    local global_failed=0
     for folder in PL*; do
         if [ -d "$folder" ]; then
             echo
-            judge_one "$folder"
+            if ! judge_one "$folder"; then
+                global_failed=1
+            fi
         fi
     done
+    return $global_failed
 }
 
 if [ $# -eq 0 ] || [[ "$1" == "all" ]]; then
-    judge_all
-elif [ -d "$1" ] || [ -d *-$1 ] || [ -d PL$1* ]; then
-    pattern="$1|[A-Za-z0-9]*-$1|PL$1[A-Za-z0-9-]*"
-    ls | grep -E ${pattern} | while read -r file; do
-        judge_one "$file"
-    done
+    if ! judge_all; then
+        exit 1
+    fi
 else
-    red "$1 is not a directory"
+    if ! [ -d "$1" ] && ! [ -d *-$1 ] && ! [ -d PL$1* ]; then
+        red "$1 is not a directory"
+        exit 1
+    fi
+    pattern="$1|[A-Za-z0-9]*-$1|PL$1[A-Za-z0-9-]*"
+    local any_failed=0
+    ls | grep -E ${pattern} | while read -r file; do
+        if ! judge_one "$file"; then
+            any_failed=1
+        fi
+    done
+    if [ $any_failed -eq 1 ]; then
+        exit 1
+    fi
 fi
 
